9ac4700b6b349129b1cc22669ddc89c21eba33a2
Dan Lough <dan.lough@soloworks.co.uk>
2018-07-14 13:13:34 +0100

    Initial commit ✔️

---
+++ src/components/App/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+const wrapper = shallow(<App />);
+
+describe('App', () => {
+  it('should render without crashing', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+});
+
